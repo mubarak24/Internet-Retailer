771a4f0480f55ed3fe2b16b73937d1691097aa7d
mubarak <mubarakcali24@gmail.com>
2023-05-19 11:51:11 -0700

    define model columns

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignkey: 'Product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_product'
+});
 
 module.exports = {
   Product,
